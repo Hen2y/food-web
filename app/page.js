ffde772e9c2457335c0e72128a6e50181b169e03
Hen2y <299348468+Hen2y@users.noreply.github.com>
2026-07-17 15:51:50 +0800

    Fix Safari auto mode camera recovery

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ export default function Home() {
     <iframe
       src="/index.html"
       title="盘安 SafePlate"
+      allow="camera"
       style={{ width: "100%", height: "100vh", border: 0, display: "block" }}
     />
   );
