aa01643b8966160bf90ed3678f13bfdb0b07573d
Hen2y <299348468+Hen2y@users.noreply.github.com>
2026-07-17 22:59:26 +0800

    Add manual retake photo option

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <iframe
-      src="/index.html?v=20260717-manual-camera-preview-fix"
+      src="/index.html?v=20260717-manual-retake-photo"
       title="盘安 SafePlate"
       allow="camera"
       style={{ width: "100%", height: "100vh", border: 0, display: "block" }}
