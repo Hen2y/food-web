b65a8c51af352eede5b10b536354c7247de61f17
Hen2y <299348468+Hen2y@users.noreply.github.com>
2026-07-17 20:44:15 +0800

    Refresh SafePlate interface styling

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <iframe
-      src="/index.html?v=20260717-profile-db"
+      src="/index.html?v=20260717-ui-refresh"
       title="盘安 SafePlate"
       allow="camera"
       style={{ width: "100%", height: "100vh", border: 0, display: "block" }}
