33ab598819ccd174084043ff2769391f03065ac2
Hen2y <299348468+Hen2y@users.noreply.github.com>
2026-07-18 14:29:34 +0800

    Strengthen blue marker auto capture block

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <iframe
-      src="/index.html?v=20260718-blue-marker-blocks-auto"
+      src="/index.html?v=20260718-blue-marker-hard-block"
       title="盘安 SafePlate"
       allow="camera"
       style={{ width: "100%", height: "100vh", border: 0, display: "block" }}
