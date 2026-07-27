9c84dcb6f11def6511433247f14283251290752d
Hen2y <299348468+Hen2y@users.noreply.github.com>
2026-07-27 12:37:02 +0800

    Restore rotating local dish backgrounds

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <iframe
-      src="/index.html?v=20260718-blue-marker-hard-block"
+      src="/index.html?v=20260727-local-dish-backgrounds"
       title="盘安 SafePlate"
       allow="camera"
       style={{ width: "100%", height: "100vh", border: 0, display: "block" }}
