3f2301eb7882b212653890e9f0381b66153765cf
Hen2y <299348468+Hen2y@users.noreply.github.com>
2026-07-28 13:28:41 +0800

    Clean school menu labels

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <iframe
-      src="/index.html?v=20260727-school-menu"
+      src="/index.html?v=20260728-menu-clean"
       title="盘安 SafePlate"
       allow="camera"
       style={{ width: "100%", height: "100vh", border: 0, display: "block" }}
