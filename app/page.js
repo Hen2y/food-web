adefc154e37c4af648ef45416b061f473b023791
Hen2y <299348468+Hen2y@users.noreply.github.com>
2026-07-30 18:05:41 +0800

    Fix school menu image loading in Safari

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <iframe
-      src="/index.html?v=20260730-menu-image-fix"
+      src="/index.html?v=20260730-menu-image-load-fix"
       title="盘安 SafePlate"
       allow="camera"
       style={{ width: "100%", height: "100vh", border: 0, display: "block" }}
